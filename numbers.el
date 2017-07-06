4a3719583bcd3904ca3bf3c76d19773aa8ec78f3
Dave Pearson <davep@davep.org>
2017-07-06 16:29:43 +0100

    v1.1

--- numbers.el
+++ numbers.el
@@ -2,7 +2,7 @@
 ;; Copyright 2017 by Dave Pearson <davep@davep.org>
 
 ;; Author: Dave Pearson <davep@davep.org>
-;; Version: 1.0
+;; Version: 1.1
 ;; Keywords: games, trivia, maths, numbers
 ;; URL: https://github.com/davep/numbers.el
 
